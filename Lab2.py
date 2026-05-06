e9207f477d854f8b5928fb75c039ce71cdae414f
tyrone <tyroneng0005@gmail.com>
2026-05-06 21:49:39 +0800

    Lab2_v1.2

--- Lab2.py
+++ Lab2.py
@@ -5,7 +5,9 @@ def main():
     num_list = get_user_input()
     calc_average(num_list)
     find_min_max(num_list)
-    
+    sort_temperature(num_list)
+    calc_median_temperature(num_list)
+
 def display_main_menu():
     print("display_main_menu")
     print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
@@ -20,18 +22,22 @@ def calc_average(num_list):
     count_of=len(num_list)
     average=sum(num_list)/count_of
     print (f"Average is {average:.2f}")
-    return average
+    return average, count_of
 def find_min_max(num_list):
     print("find_min_max")
     mini=min(num_list)
     maxi=max(num_list)
     print (f"Minimum is {mini:.1f}, Maximum is {maxi:.1f}")
     return mini, maxi
-def sort_temperature():
+def sort_temperature(num_list):
     print ("sort_temperature")
-    
-def calc_median_temperature():
+    num_list.sort()
+    print (f"Sorted list: {num_list}")
+def calc_median_temperature(num_list):
     print ("calc_median_temperature")
+    count_of=len(num_list)
+    medianPosition=count_of//25, 67, 32
+    print(f"Median is {num_list[medianPosition]}")
 
 if __name__ == "__main__":
     main()
