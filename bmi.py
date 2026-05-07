7d402a5bf4e93f0a769f3c0a34b5e2c7f25737a7
tyrone <tyroneng0005@gmail.com>
2026-05-07 11:01:50 +0800

    edited bmi for testing

--- bmi.py
+++ bmi.py
@@ -5,8 +5,11 @@ def calculate_bmi(height, weight):
     print (f"BMI is {bmi:.2f}")
     if bmi<18.5:
         print("Under Weight")
+        return -1
     elif bmi<=25.0:
         print("Normal Weight")
+        return 0
     elif bmi>25.0:
         print("Over Weight")
+        return 1
 calculate_bmi(weight=57, height=1.73)
